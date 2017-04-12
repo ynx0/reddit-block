f08548f4fc30857c8525487e57fe50d0e688e455
Yaseen S <ThePhantomGamer@users.noreply.github.com>
2017-04-12 19:11:10 -0400

    main code
    added the logic.
    basically, it runs at document-start, which is before the page loads any images, and then redirects to reddit.com/r/getoffreddit on any page matching `http://*.reddit.com/*`

--- rblock.user.js
+++ rblock.user.js
@@ -1,1 +1,18 @@
-// redirects all reddit traffic to https://www.reddit.com/r/getoffreddit/
+// ==UserScript==
+// @name         reddit-block
+// @namespace    https://github.com/ThePhantomGamer/reddit-block
+// @version      0.1
+// @description  redirect all reddit.com traffic -> /r/getoffreddit
+// @author       ThePhantomGamer
+// @match        http://*.reddit.com/*
+// @grant        none
+// @run-at document-start
+// ==/UserScript==
+
+(function() {
+  'use strict';
+  window.location.replace('reddit.com/r/getoffreddit');
+  
+})();
+
+
