66b0ef1335ec00cafd78696e833df0d4e89ed6c5
Yaseen S <ThePhantomGamer@users.noreply.github.com>
2017-04-12 19:12:05 -0400

    Added `https` match

--- rblock.user.js
+++ rblock.user.js
@@ -5,6 +5,7 @@
 // @description  redirect all reddit.com traffic -> /r/getoffreddit
 // @author       ThePhantomGamer
 // @match        http://*.reddit.com/*
+// @match        https://*.reddit.com/*
 // @grant        none
 // @run-at document-start
 // ==/UserScript==
